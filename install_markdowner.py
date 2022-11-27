7f894f7a83aa12b5042fc7946070ce6fa4509df7
Tizian Erlenberg <accounts@erlenberg.net>
2022-11-27 23:42:15 +0100

    first commit

---
+++ install_markdowner.py
@@ -0,0 +1,122 @@
+import subprocess as sp
+import platform
+import installer
+import sys
+
+class globVars():
+    pass
+
+class CustomError(Exception):
+    pass
+
+def checkWslStatus():
+    cmd = "wsl -d ubuntu echo WSL INSTALLED"
+    sess = sp.Popen(cmd,
+                    shell=True,
+                    stdout=sp.PIPE,
+                    stderr=sp.PIPE)
+    rc = sess.wait()
+    out,err=sess.communicate()
+    cmd = "wsl -d debian echo WSL INSTALLED"
+    sess = sp.Popen(cmd,
+                    shell=True,
+                    stdout=sp.PIPE,
+                    stderr=sp.PIPE)
+    rc = sess.wait()
+    out2,err2=sess.communicate()
+
+    if out.decode().startswith('WSL INSTALLED'):
+        return 'UBUNTU'
+    elif out2.decode().startswith('WSL INSTALLED'):
+        return 'DEBIAN'
+    
+    return 'NOT DEFINED'
+
+def checkAdminStatus():
+    cmd = "net session"
+    sess = sp.Popen(cmd,
+                    shell=True,
+                    stdout=sp.PIPE,
+                    stderr=sp.PIPE)
+    rc = sess.wait()
+    out,err=sess.communicate()
+
+    if err.decode().startswith('System error 5 has occurred.'):
+        return False
+    elif out.decode().startswith('There are no entries in the list.'):
+        return True
+    else:
+        raise Exception("Can't determine administrator status") 
+
+def installWsl():
+    if checkWslStatus() == 'NOT DEFINED':
+        message = ('The System detected that you are running a Windows machine. '+
+                   'It will try to install the Windows Subsystem for Linux.')
+        if not installer.continueYesNo(message):
+            print("Aborting installation")
+            sys.exit(1)
+        if not checkAdminStatus():
+            input('To install the Windows Subsystem for Linux the installer needs admin privileges.' + 
+                'Please run the program with administrator privileges.' + 
+                'To exit press ENTER: ')
+            sys.exit(1)
+        
+        input('The Program will now install WSL for you.'+
+            'This may take a while.\n\n'+
+            '============== IMPORTANT ==============\n' +
+            'After the install of Windows Subsystem for Linux you may have to restart the computer.' + 
+            'You will be asked to enter a UNIX username and password after the reboot' +
+            'After that happened, open this installer again.\n\n' +
+            'To continue press ENTER: ')
+        cmd = "wsl --install -d ubuntu"
+        sp.check_call(cmd, shell=True)
+        input('\nWindows Subsystem for Linux successfully installed.\n\n' +
+            'PLEASE RESTART THE COMPUTER NOW' +
+            'To continue press ENTER: ')
+        sys.exit(0)
+    else:
+        return 0
+
+def installWslTools():
+    prefix = ''
+    wslType = checkWslStatus()
+    if wslType == 'UBUNTU':
+        prefix = 'wsl -d ubuntu'
+    elif wslType == 'DEBIAN':
+        prefix = 'wsl -d debian'
+    else:
+        raise Exception('WSL not installed')
+    
+    input('The program will install the required tools now. '+
+          'This may take a while.\n'+
+          'To continue please press ENTER: ')
+    
+    password = input('Please enter the password you use for the WSL, '+
+                     'so this program can install the necessary tools: ')
+    
+    cmd = f'{prefix} bash -c "echo {password} | sudo -S apt install pandoc npm texlive-full"'
+    sp.check_call(cmd, shell=True)
+
+    cmd = f'{prefix} bash -c "npm install --save-dev --save-exact prettier"'
+    sp.check_call(cmd, shell=True)
+
+def installAll():
+    if platform.system() == "Windows":
+        def beforeInstall():
+            retVal = installWsl()
+            if retVal == 0:
+                installWslTools()
+            else:
+                raise Exception('Something went wrong during the installation of the WSL')
+    else:
+        def beforeInstall():
+            pass
+            #TODO
+    
+    installer.interactiveInstall('markdowner', beforeInstall=beforeInstall, addToPathVar=True)
+
+def main():
+    installAll()
+
+if __name__ == '__main__':
+    main()
---
+++ installer.py
@@ -0,0 +1,145 @@
+from pathlib import Path
+from zipfile import ZipFile
+import os, sys, platform
+
+def getInternalFilePath(nameOfZip):
+    try:
+        wd = sys._MEIPASS
+    except AttributeError:
+        wd = os.getcwd()
+    return os.path.join(wd, nameOfZip)
+
+def getInstallPath(nameOfProgram):
+    if platform.system() == "Windows":
+        return Path(os.path.expanduser('~')) / Path('AppData/Roaming' / Path(nameOfProgram))
+    else:
+        return Path(os.path.expanduser('~')) / Path("." + nameOfProgram)
+
+def extractBlob(name, dest):
+    with ZipFile(getInternalFilePath(name)) as zObject:
+        zObject.extractall(path=dest)
+
+def addToPath(name):
+    import subprocess as sp
+    if platform.system() == "Windows":
+        #powershell -Command "[Environment]::SetEnvironmentVariable('PATH', \"$([Environment]::GetEnvironmentVariable('PATH', 'User'));C:\Users\Tizian Erlenberg\AppData\Roaming\markdowner\",'User')"
+        cmd =   ("powershell -Command \"" +
+                    "[Environment]::SetEnvironmentVariable(" +
+                        "'PATH', \\\"" + 
+                            "$([Environment]::GetEnvironmentVariable(" +
+                                "'PATH', 'User'" +
+                            "))" +
+                            ";" +
+                            str(getInstallPath(name)) +
+                        "\\\"" +
+                        ", " +
+                        "'User'" +
+                    ")" +
+                "\"")
+        sp.check_call(cmd, shell=True)
+    else:
+        pass
+        #TODO
+
+def setDoneFlag(nameOfProgram):
+    with open(getInstallPath(nameOfProgram) / Path('installerHasRun'), 'w') as fp:
+        pass
+
+def unsetDoneFlag(nameOfProgram):
+    os.remove(getInstallPath(nameOfProgram) / Path('installerHasRun'))
+
+def isAlreadyInstalled(nameOfProgram):
+    if os.path.exists(getInstallPath(nameOfProgram) / Path('installerHasRun')):
+        return True
+    else:
+        return False
+
+def install(nameOfProgram, addToPathVar=False, setFlag=True):
+    if isAlreadyInstalled(nameOfProgram):
+        return 1
+
+    extractBlob("package.zip", getInstallPath(nameOfProgram))
+    if addToPathVar:
+        addToPath(nameOfProgram)
+    if setFlag:
+        setDoneFlag(nameOfProgram)
+    return 0
+
+def uninstall(nameOfProgram):
+    import subprocess as sp
+    if platform.system() == "Windows":
+        cmd = "powershell -Command \"[Environment]::GetEnvironmentVariable('PATH', 'User')\""
+        sess = sp.Popen(cmd,
+                    shell=True,
+                    stdout=sp.PIPE,
+                    stderr=sp.PIPE)
+        rc = sess.wait()
+        out,err=sess.communicate()
+        listOfEntrys = out.decode().split(';')
+        newListOfEntrys = listOfEntrys.copy()
+        
+        for item in listOfEntrys:
+            if str(getInstallPath(nameOfProgram)) in item:
+                newListOfEntrys.remove(item)
+
+        newPath = ';'.join(newListOfEntrys)
+        cmd =   ("powershell -Command \"" +
+                    "[Environment]::SetEnvironmentVariable(" +
+                        "'PATH', \\\"" + 
+                            newPath +
+                        "\\\"" +
+                        ", " +
+                        "'User'" +
+                    ")" +
+                "\"")
+        sp.check_call(cmd, shell=True)
+        unsetDoneFlag(nameOfProgram)
+    else:
+        pass
+        #TODO
+
+def continueYesNo(message):
+    user_input = input(f"{message}\n" + 
+                        "Do you want to continue? (y / n): ")
+
+    if user_input == 'y' or user_input == 'Y':
+        return True
+    else:
+        return False
+
+def interactiveInstall(nameOfProgram, addToPathVar=False, beforeInstall=None, afterInstall=None):
+    if isAlreadyInstalled(nameOfProgram):
+        message = ('Program is already installed on your computer.\n' +
+                   'Therefore the uninstaller is beeing started.\n\n' +
+                   '============= UNISNTALLER =============\n' +
+                   f'Do you REALLY want to uninstall \"{nameOfProgram}\" from your system?')
+        if continueYesNo(message):
+            uninstall(nameOfProgram)
+        sys.exit(1)
+
+    message = (f"This is an installer for {nameOfProgram}. " + 
+               "It will install the Program on your computer.")
+    if not continueYesNo(message):
+        print("Aborting installation")
+        return 1
+
+    if beforeInstall:
+        beforeInstall()
+    
+    install(nameOfProgram, addToPathVar=addToPathVar, setFlag=False)
+
+    if afterInstall:
+        afterInstall()
+    
+    setDoneFlag(nameOfProgram)
+    input("Program successfully installed. To exit press ENTER: ")
+    return 0
+
+def main():
+    pass
+    #interactiveInstall('testProgram')
+    uninstall('markdowner')
+
+if __name__ == '__main__':
+    main()
+
